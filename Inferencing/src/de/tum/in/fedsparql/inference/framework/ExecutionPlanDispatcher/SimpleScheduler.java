00930a10ec39a9943b0d9183c0081277b83010a6
Dominik Weber <d.weber@edo-soft.com>
2012-06-20 06:51:31 +0200

    -

--- Inferencing/src/de/tum/in/fedsparql/inference/framework/ExecutionPlanDispatcher/SimpleScheduler.java
+++ Inferencing/src/de/tum/in/fedsparql/inference/framework/ExecutionPlanDispatcher/SimpleScheduler.java
@@ -5,6 +5,11 @@ import de.tum.in.fedsparql.inference.framework.ScriptCollection;
 
 public class SimpleScheduler extends Scheduler {
 
+	public SimpleScheduler(ScriptCollection collection) {
+		super(collection);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public void enqueue(Script script, ScriptCollection collection) {
 		System.out.println("DISPATCHER: executing " + script);
