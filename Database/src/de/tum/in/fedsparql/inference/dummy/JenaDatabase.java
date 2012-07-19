564c74ea0010b10a535f9e92df51c6e3020cb250
Dominik Weber <d.weber@edo-soft.com>
2012-07-19 12:59:26 +0200

    bugfix: equals method of JenaDatabase missing

--- Database/src/de/tum/in/fedsparql/inference/dummy/JenaDatabase.java
+++ Database/src/de/tum/in/fedsparql/inference/dummy/JenaDatabase.java
@@ -4,4 +4,24 @@ public class JenaDatabase extends DummyDatabase {
 	public JenaDatabase(String name) {
 		super(name);
 	}
+	
+	@Override
+	public String toString() {
+		return this.getName();
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof JenaDatabase) {
+			JenaDatabase db = (JenaDatabase) obj;
+			return db.getName() != null ? db.getName().equals(this.getName()) : this.getName() == null;
+		}
+		
+		return false;
+	}
 }
