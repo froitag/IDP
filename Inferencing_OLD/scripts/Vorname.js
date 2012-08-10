62c9672a45467338da2d5474728ee3a23643660f
Andre Freitag <ratatatata@gmx.net>
2012-08-10 08:52:50 +0200

    removed old files

--- Inferencing_OLD/scripts/Vorname.js
+++
@@ -1,7 +0,0 @@
-{
-	name: 'Vorname',
-	script: function ($val) {
-		var x = $val.substring(1,$val.length-1).split(' ');
-		return '"'+x[0]+'"';
-	}
-};
--- Inferencing_OLD/src/de/tum/in/fedsparql/inference/framework/JS/JSObject.java
+++
@@ -1,6 +0,0 @@
-package de.tum.in.fedsparql.inference.framework.JS;
-
-public class JSObject {
-	public String getTitle() {return "test";}
-	public int counter=0;
-}
